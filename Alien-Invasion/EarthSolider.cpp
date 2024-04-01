31826c3e1120293661bf1d494b6c9d86e4f6b97a
amirakhaled928 <amirakhaled928@gmail.com>
2024-04-01 02:15:23 +0200

    Adding first version of unit classes

---
+++ Alien-Invasion/EarthSolider.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#include "EarthSolider.h"
+
+EarthSolider::EarthSolider()
+{
+
+}
+
+void EarthSolider::Attack()
+{
+
+}
+
+EarthSolider::~EarthSolider()
+{
+}
---
+++ Alien-Invasion/EarthSolider.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Unit.h"
+class EarthSolider : public Unit
+{
+public:
+	EarthSolider();
+	virtual void Attack();
+	~EarthSolider();
+
+};
+
